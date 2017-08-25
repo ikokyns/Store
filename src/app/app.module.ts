862c1518f1430118a12ec5e2b6fa6e3ac330066f
ikokyns <momcilo87@hotmail.com>
2017-08-25 19:34:02 +0200

    Zadatak 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { ProductsComponent } from './components/products/products.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/buyers',
+    pathMatch: 'full'
+  },
+  {
+    path: 'buyers',
+    component: BuyersComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BuyersComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+        appRoutes
+      )
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+export class AppRoutingModule { }
---
+++ src/app/components/buyers/buyers.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buyers-component',
+  templateUrl: './buyers.component.html'
+})
+export class BuyersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
